481335678b0040f4bb601b1a881fefe9e4b2349b
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2021-05-06 13:59:00 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000;
-
+#test
 app.listen(port);
 console.log(`Acesse http://localhost: ${port}`);
 app.get('/', (req, res) => {
